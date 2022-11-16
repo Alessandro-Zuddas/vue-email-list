6be1a7ae083ecde9678fae5410d2932b2903a5b2
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-16 15:18:44 +0100

    Agigunge stampa delle email prese dal server

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            mails: [
+
+            ]
+        }
+    },
+    created(){
+        for(let i = 0; i < 10; i++){
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then((response) =>{
+                const result = response.data;
+                this.mails.push(result.response);
+            });
         }
     }
 }).mount('#app');
